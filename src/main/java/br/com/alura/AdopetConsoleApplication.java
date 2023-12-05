1524a5c4a7af22e71a5b41e9ce3e25b015a5a795
murilomts <murilo.mtds@gmail.com>
2023-12-04 21:02:20 -0300

    refatoração já iniciada, criado pacote client e service

---
+++ src/main/java/br/com/alura/AdopetConsoleApplication.java
@@ -0,0 +1,50 @@
+package br.com.alura;
+
+import br.com.alura.client.ClientHttpConfiguration;
+import br.com.alura.service.AbrigoService;
+import br.com.alura.service.PetService;
+import java.util.Scanner;
+
+public class AdopetConsoleApplication {
+
+    public static void main(String[] args) {
+        ClientHttpConfiguration clientHttpConf = new ClientHttpConfiguration();
+        AbrigoService abrigoService = new AbrigoService(clientHttpConf);
+        PetService petService = new PetService(clientHttpConf);
+
+        System.out.println("##### BOAS VINDAS AO SISTEMA ADOPET CONSOLE #####");
+        try {
+            int opcaoEscolhida = 0;
+            while (opcaoEscolhida != 5) {
+                System.out.println("\nDIGITE O NÚMERO DA OPERAÇÃO DESEJADA:");
+                System.out.println("1 -> Listar abrigos cadastrados");
+                System.out.println("2 -> Cadastrar novo abrigo");
+                System.out.println("3 -> Listar pets do abrigo");
+                System.out.println("4 -> Importar pets do abrigo");
+                System.out.println("5 -> Sair");
+
+                String textoDigitado = new Scanner(System.in).nextLine();
+                opcaoEscolhida = Integer.parseInt(textoDigitado);
+
+                if (opcaoEscolhida == 1) {
+                    abrigoService.listarAbrigos();
+                } else if (opcaoEscolhida == 2) {
+                    abrigoService.cadastrarAbrigo();
+                } else if (opcaoEscolhida == 3) {
+                    petService.listarPets();
+                } else if (opcaoEscolhida == 4) {
+                    petService.importarPets();
+                } else if (opcaoEscolhida == 5) {
+                    break;
+                } else {
+                    System.out.println("NÚMERO INVÁLIDO!");
+                    opcaoEscolhida = 0;
+                }
+            }
+            System.out.println("Finalizando o programa...");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
